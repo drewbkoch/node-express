69d3d390de15f3732263a938f0777fd1514cb8df
Drew Koch <dkoch13@gmail.com>
2014-04-16 09:57:27 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/foo', function(req, res) {
+  res.render('foo', { title: 'Express' });
+});
+
+module.exports = router;
